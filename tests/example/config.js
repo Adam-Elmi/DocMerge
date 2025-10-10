d69f46cd473297b9c8c288367c42b9a1b51367ec
Adam-Elmi <Adamcade123@gmail.com>
2025-10-10 23:21:59 +0300

    Update config.js

--- tests/example/config.js
+++ tests/example/config.js
@@ -1,5 +1,7 @@
+import fs from "node:fs/promises";
+import { getPath } from "../../helpers/getPath.js";
 export default {
-  imports: ["import somcheat_api from 'somcheat'"],
+  imports: ["import somcheat from 'somcheat'"],
   frontmatter: `---
   name: Adam Elmi Eid
   id: 101
@@ -8,4 +10,17 @@ export default {
   outputFile: "js.mdx",
   outputDir: "./tests/generated",
   order: [],
+  hook: async(path, files) => {
+    for (const file of files) {
+      const content = await fs.readFile(getPath(process.argv[2], file));
+      if(file === "1.step_one.md") {
+        await fs.appendFile(path, `<somcheat>\n${content}` + "\n");
+      } else if(file === "6.step_six.md") {
+        await fs.appendFile(path, `${content}\n</somcheat>`);
+      }
+      else {
+        await fs.appendFile(path, content + "\n");
+      }
+    }
+  }
 }
