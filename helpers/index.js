97682afe5f6820657a7cffce67cd38b83b52275f
Adam-Elmi <Adamcade123@gmail.com>
2025-10-11 16:35:17 +0300

    Create index.js

---
+++ helpers/index.js
@@ -0,0 +1,19 @@
+import fileExists from "./file_exists.js";
+import getPath from "./get_path.js";
+import check_type_value from "./check_value_type.js";
+import get_properties_info from "./get_properties_info.js";
+import is_includes from "./is_includes.js";
+import read_directory from "./read_directory.js";
+import add_contents from "./add_contents.js";
+import generate_file from "./generate_file.js";
+
+export {
+  fileExists,
+  getPath,
+  check_type_value,
+  get_properties_info,
+  is_includes,
+  read_directory,
+  add_contents,
+  generate_file
+};
