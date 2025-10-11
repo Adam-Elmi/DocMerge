31bc4c3842662e37a73f7f815f73f23d422faa56
Adam-Elmi <Adamcade123@gmail.com>
2025-10-11 16:34:56 +0300

    Update run_tests.js

--- tests/unit_test/run_tests.js
+++ tests/unit_test/run_tests.js
@@ -2,25 +2,30 @@ import os from "node:os";
 import {
   fileExists,
   getPath,
-  is_includes,
   check_type_value,
-} from "../merge.test.js";
-
-function expect_to_be_equal(
-  value1,
-  value2,
-  task = "",
-) {
+  get_properties_info,
+  is_includes,
+  read_directory,
+  add_contents,
+  generate_file,
+} from "../../helpers/index.js";
+const config_properties = [
+  "imports",
+  "frontmatter",
+  "outputFile",
+  "outputDir",
+  "order",
+  "hook",
+];
+function expect_to_be_equal(value1, value2, task = "") {
   const passed =
-    "\x1b[32mTEST PASSED:\x1b[0m \x1b[36mTRUE\x1b[0m " + "-> " +
-    (task
-      ? `\x1b[33mTASK:\x1b[0m ${task}`
-      : task);
+    "\x1b[32mTEST PASSED:\x1b[0m \x1b[36mTRUE\x1b[0m " +
+    "-> " +
+    (task ? `\x1b[33mTASK:\x1b[0m ${task}` : task);
   const failed =
-    "\x1b[31mTEST FAILED:\x1b[0m \x1b[35mFALSE\x1b[0m " + "-> " +
-    (task
-      ? `\x1b[33mTASK:\x1b[0m: ${task}`
-      : task);
+    "\x1b[31mTEST FAILED:\x1b[0m \x1b[35mFALSE\x1b[0m " +
+    "-> " +
+    (task ? `\x1b[33mTASK:\x1b[0m: ${task}` : task);
   if (value1 === value2) {
     console.log(passed);
   } else {
@@ -38,7 +43,11 @@ console.log("\x1b[34m[--Testing function:\x1b[0m \x1b[36mfileExists--]\x1b[0m");
   ---------------
 */
 const is_file_1_exists = await fileExists("./tests/example/config.js");
-expect_to_be_equal(is_file_1_exists, true, "Check if file exists using a real path");
+expect_to_be_equal(
+  is_file_1_exists,
+  true,
+  "Check if file exists using a real path",
+);
 /*
   ---------------
   Check if file exists using a fake path
@@ -46,7 +55,11 @@ expect_to_be_equal(is_file_1_exists, true, "Check if file exists using a real pa
   ---------------
 */
 const is_file_2_exists = await fileExists("./example/docs/config.js");
-expect_to_be_equal(is_file_2_exists, false, "Check if file exists using a fake path");
+expect_to_be_equal(
+  is_file_2_exists,
+  false,
+  "Check if file exists using a fake path",
+);
 /*
   ---------------
   Check if file exists if path is not defined
@@ -54,7 +67,11 @@ expect_to_be_equal(is_file_2_exists, false, "Check if file exists using a fake p
   ---------------
 */
 const is_file_3_exists = await fileExists();
-expect_to_be_equal(is_file_3_exists, false, "Check if file exists if path is not defined");
+expect_to_be_equal(
+  is_file_3_exists,
+  false,
+  "Check if file exists if path is not defined",
+);
 
 console.log("-".repeat(30));
 console.log("\x1b[34m[--Testing function:\x1b[0m \x1b[36mgetPath--]\x1b[0m");
@@ -67,10 +84,18 @@ console.log("\x1b[34m[--Testing function:\x1b[0m \x1b[36mgetPath--]\x1b[0m");
 */
 if (os.platform === "win32") {
   const path_1 = getPath(".\\example\\\\", "config.js");
-  expect_to_be_equal(path_1, ".\\example\\config.js", "Check if it returns an accurate path (windows path)");
+  expect_to_be_equal(
+    path_1,
+    ".\\example\\config.js",
+    "Check if it returns an accurate path (windows path)",
+  );
 } else {
   const path_2 = getPath("./example////", "config.js");
-  expect_to_be_equal(path_2, "./example/config.js", "Check if it returns an accurate path (unix-like path)");
+  expect_to_be_equal(
+    path_2,
+    "./example/config.js",
+    "Check if it returns an accurate path (unix-like path)",
+  );
 }
 console.log("-".repeat(30));
 console.log(
@@ -83,7 +108,11 @@ console.log(
   ---------------
 */
 const config_object = await import("../example/config.js");
-expect_to_be_equal(is_includes(config_object.default), true, "Check if config_object includes allowed properties");
+expect_to_be_equal(
+  is_includes(config_object.default, config_properties),
+  true,
+  "Check if config_object includes allowed properties",
+);
 /*
   ---------------
   Check if config_object does not include allowed properties
@@ -91,7 +120,11 @@ expect_to_be_equal(is_includes(config_object.default), true, "Check if config_ob
   ---------------
 */
 config_object.default.id = 101;
-expect_to_be_equal(is_includes(config_object.default), false, "Check if config_object does not include allowed properties");
+expect_to_be_equal(
+  is_includes(config_object.default, config_properties),
+  false,
+  "Check if config_object does not include allowed properties",
+);
 
 console.log("-".repeat(30));
 console.log(
